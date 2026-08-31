02862dc10f1cd70bf7c15890398f03416fa931b4
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-08-31 23:01:09 +0200

    1. The last digit

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -8,8 +8,8 @@ if number < 0:
     last_digit = last_digit * -1
 
 if last_digit > 5:
-    print("last_digit of", number, "and is greater than 5")
-elif last_digit == 0: 
-    print("Last digit of", number, "is", last_digit, "and is 0") 
-else: 
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
+else:
     print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
