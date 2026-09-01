a4e52ab6062673d2309d24a9df07af239082c8ee
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-01 10:56:51 +0200

    2. Alphabet Game (Lowercase)

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
-for i in range(97, 123):
-    if i != 101 and i != 113:
-        print("%c" % i, end="")
+alphabet = ""
+
+for letter in range(97, 123):
+    character = chr(letter)
+    if character != "e" and character != "q":
+        alphabet += character
+
+print("{}".format(alphabet), end="")
