90dce8eec2864240397f9f643fd4e34dc49d419d
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-02 13:26:04 +0200

    1. To Uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    result = ""
+    for char in str:
+        if ord(char) >= ord('a') and ord(char) <= ord('z'):
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print(result)
+    
