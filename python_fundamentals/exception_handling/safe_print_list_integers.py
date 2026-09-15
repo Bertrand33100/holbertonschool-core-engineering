bd081259949dee7ca78b145d33b184d7d2cb3b27
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-15 15:42:25 +0200

    2. Safe list printing with type handling

--- python_fundamentals/exception_handling/safe_print_list_integers.py
+++ python_fundamentals/exception_handling/safe_print_list_integers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def safe_print_list_integers(my_list=[], x=0):
+    """Print the first x integers of a list."""
 
     count = 0
 
@@ -8,7 +9,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        except ValueError:
+        except (ValueError, TypeError):
             pass
 
     print()
