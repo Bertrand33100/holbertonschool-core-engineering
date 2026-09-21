3071fa0e5d683d0812badb3a9599c19d4949f21c
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-21 12:17:51 +0200

    Ajout des docstring

--- python_oop/classes_and_object_model/1-square.py
+++ python_oop/classes_and_object_model/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""Documentation du modile."""
+
 class Square:
+    """Documentation de la classe."""
+
     def __init__(self, size):
+        """Documentation du constructeur."""
         self.__size = size
