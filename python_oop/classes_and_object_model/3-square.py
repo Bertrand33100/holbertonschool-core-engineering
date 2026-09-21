6ba2dab014afc0034006a8ef3468210295f00cb3
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-21 13:07:28 +0200

    4. Area of a Square

---
+++ python_oop/classes_and_object_model/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Documentation du module."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a Square with a validated size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area."""
+        return self.__size * self.__size
