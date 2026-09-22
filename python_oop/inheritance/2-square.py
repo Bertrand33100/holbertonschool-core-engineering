5e4549bcce40e16f9a57706c42b32cc8c9fd763f
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-22 21:56:50 +0200

    Square #2

---
+++ python_oop/inheritance/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Defines the Square class."""
+
+Rectangle = __import__('2-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square, a specialized rectangle."""
+
+    def __init__(self, size):
+        """Initialize a square with a given size."""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Return a readable string representation of the square."""
+        return "[Square] {}/{}".format(self.width, self.height)
