c3f3e459e4c552bbd236c66d7af34e4b0c5ff902
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-23 15:03:08 +0200

    Fix formulation "2" lin 28

--- python_oop/abc/shapes.py
+++ python_oop/abc/shapes.py
@@ -25,7 +25,7 @@ class Circle(Shape):
         return math.pi * self.radius ** 2
 
     def perimeter(self):
-        return math.pi * self.radius
+        return 2 * math.pi * self.radius
 
 class Rectangle(Shape):
     """Represent a rectangle."""
