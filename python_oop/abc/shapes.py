307b02c0337e20ef5cc4b1b1c5659daacb601457
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-23 14:58:00 +0200

    1. Shapes, Interfaces, and Duck Typing

---
+++ python_oop/abc/shapes.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+from abc import ABC, abstractclassmethod
+import math
+
+
+class Shape(ABC):
+    """Abstarct class repreenting a Shape;"""
+
+    @abstractclassmethod
+    def are(self):
+        """Calculate the are."""
+        pass
+    @abstractclassmethod
+    def perimeter(self):
+        pass
+
+class Circle(Shape):
+    """Represent a circle."""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return math.pi * self.radius
+
+class Rectangle(Shape):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """Print the area and perimeter of a shape."""
+    print("Area:", shape.area())
+    print("Perimeter:", shape.perimeter())
