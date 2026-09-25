54226c2e2bcbc38fc14e7b8b84c0628e04cc2555
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-25 16:45:55 +0200

    0. Read file

---
+++ python_advanced/read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Pour lire et afficcher le texte du fichier"""
+
+
+def read_file(filename=""):
+    """lire le text (utf8) et affiche la sortie"""
+    with open(filename, encoding="utf-8") as f:
+        read = f.read()
+    print(read)
