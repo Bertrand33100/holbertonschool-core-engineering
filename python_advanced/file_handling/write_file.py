b150d5ae8968ba702932c9e6b89ee30eb25100d1
Bertrand Oeung <bertrand.oeung@hotmail.fr>
2026-09-25 23:30:02 +0200

    1. Write to a file

---
+++ python_advanced/file_handling/write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that writes a string to a text file."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a text file (UTF8) and return chars written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        write = f.write(text)
+    return write
